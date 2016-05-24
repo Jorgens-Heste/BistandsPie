fd3aa4a9514cc25a8a61c9bf69b84404a87180a3
G74un3 <daniel@graungaard.com>
2016-05-24 16:58:58 +0200

    Started integration sesion class added

--- src/interaction.py
+++ src/interaction.py
@@ -1,14 +1,35 @@
+class Session(object):
+    sessionNumber = "0"
+    elements = ()
+    heading = "velkommen"
+
+
+    def setSessionNumber(self, number):
+        self.sessionNumber = number
+
+    def setUser(self, person):
+        self.person = person
+
+    def appendElement(self, cssclass, content):
+        item = (cssclass, content)
+        self.elements.append(item)
+
+    def setHeading(self, heading):
+        self.heading = heading
+
 
 
 class InteractionManager(object):
 
-    session = "0"
+    session = Session()
 
     def setUser(self, user):
         self.user = user
 
-
     def sendUserToSession(self, previoussession):
+
+        self.session = Session()
+        self.session.setUser(self.user)
 
         if previoussession == 0:
             self.startSession1()
@@ -19,17 +40,21 @@ class InteractionManager(object):
 
 
     def startSession1(self):
-        self.session = "session-1"
+        self.session.setSessionNumber("1")
+        self.session.setHeading("Afklaring")
+
         print "Starter session: 1"
 
 
     def startSession2(self):
-        self.session = "session-2"
+        self.session.setSessionNumber("2")
+        self.session.setHeading("Valg")
+
         print "Starter session: 2"
 
-
     def startSession3(self):
-        self.session = "session-3"
+        self.session.setSessionNumber("3")
+        self.session.setHeading("Fremtid")
         print "Starter session: 3"
 
     def getContent(self):
