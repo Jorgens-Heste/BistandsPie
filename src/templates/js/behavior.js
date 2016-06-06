c092be1eb1f601acbc687a6b2c367e3b282b3851
mflarup@gmail.com <mflarup@gmail.com>
2016-06-06 22:18:32 +0200

    Updated the style.css and behavior.js files with the code from the examplehtml files

--- src/templates/js/behavior.js
+++ src/templates/js/behavior.js
@@ -3,10 +3,10 @@ $( document ).ready(function() {
 
 
     var newpositions = [
-    {right: 1300, top: 700},
+        {right: 1300, top: 700},
     {right: 400, top: 800},
     {right: 1000, top: 500},
-    {right: 400, top: 200},
+    {right: 400, top: 215},
     {right: 500, top: 1050},
     {right: 1075, top: 700},
     {right: 500, top: 450},
@@ -18,7 +18,7 @@ $( document ).ready(function() {
     {right: 1350, top: 200},
     {right: 700, top: 600},
     {right: 1025, top: 250},
-    {right: 150, top: 175},
+    {right: 75, top: 250},
     {right: 0, top: 0},
     {right: 0, top: 0},
     {right: 0, top: 0},
@@ -29,7 +29,6 @@ $( document ).ready(function() {
     {right: 0, top: 0},
     {right: 0, top: 0},
     {right: 0, top: 0}]
-
     var counter = 0;
 
     $(".green, .red, .blue, .question").each(function(index) {
